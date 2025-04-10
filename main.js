c4b8e2836cf713ade9a92dc78df7551e70bf4851
pioliveirac <oliveiracavalcantipietra@gmail.com>
2025-04-09 22:16:36 -0300

    início da lógica dos botões

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0 
+let buttonSelected = null 
 
 function receiveBillValue() {
-    bill = document.querySelector(#bill).valueAsNumber
+    bill = document.querySelector("#bill").valueAsNumber
 }
 
 function receiveNumberOfPeopleValue() {
-    numberOfPeoples = document.querySelector(#people).valueAsNumber
+    numberOfPeoples = document.querySelector("#people").valueAsNumber
+}
+
+function receiveTipPercentageValue(value){
+    
+    tipPercentage = value / 100
+
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
 }
